301daa00f20375b598947f14312321f59da558f6
kemalbekcan <kemalbekcan@gmail.com>
2022-03-11 21:21:46 +0300

    Create addproperty button

--- client/components/Buttons/AddProperty.tsx
+++ client/components/Buttons/AddProperty.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import Props from "../../interfaces/ButtonInterface";
 
-console.log("asda")
 const AddProperty: React.FC<Props> = ({ message }) => (
   <button className="property-button">
     <span className="property-button__plus">plus</span>
