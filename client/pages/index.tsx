48f8b6b8ca392a7f15321c9c638b2e56d381bfa9
kemalbekcan <kemalbekcan@gmail.com>
2022-03-16 22:58:07 +0300

    Added home page html

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -97,7 +97,119 @@ const Home: NextPage = () => {
                     </button>
                   <button className='landing-page-content-filter-form-search__search-button'>Search</button>
                 </div>
+                <div className='landing-page-content-filter-form-search-info'>
+                  <h3>We’ve more than <span>54,000</span> apartments, place & plot.​</h3>
+                </div>
               </form>
+            </div>
+            <div className='landing-page-content-clients'>
+              <div className='landing-page-content-clients-left'>
+                <span className='landing-page-content-clients-left__sub-title'>OUR CLIENTS</span>
+                <h2 className='landing-page-content-clients-left__title'>
+                  We're going to became partners for the long run
+                </h2>
+                <p className='landing-page-content-clients-left__description'>Ghen an unknown printer took a galley of type andscr ambledit to make a type specimen book has survived not only five centuries but also.</p>
+              </div>
+              <div className='landing-page-content-clients-left-right'>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+            </div>
+            <div className='landing-page-content-properties'>
+              <div>
+                <div>
+                  <span className='landing-page-content-properties__sub-title'>
+                    OUR PROPERTIES
+                  </span>
+                  <h2 className='landing-page-content-properties__title'>Latest Properties</h2>
+                </div>
+                <div>
+                  <button></button>
+                  <button></button>
+                  <button></button>
+                </div>
+              </div>
+              <div className='landing-page-content-properties-cards'>
+                <div className='landing-page-content-properties-cards-card'>
+                  <img className='landing-page-content-properties-cards-card__image' src="" alt="" />
+                  <div className='landing-page-content-properties-cards-card-content'>
+                    <h2 className='landing-page-content-properties-cards-card-content__title'></h2>
+                    <p className='landing-page-content-properties-cards-card-content__description'></p>
+                  </div>
+                  <div className='landing-page-content-properties-cards-card-content-properties'>
+                    <div className='landing-page-content-properties-cards-card-content-properties-beds'></div>
+                    <div className='landing-page-content-properties-cards-card-content-properties-baths'></div>
+                    <div className='landing-page-content-properties-cards-card-content-properties-sqft'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='landing-page-content-video'>
+              <div>
+                <video src=""></video>
+              </div>
+              <div>
+                <span></span>
+                <h2></h2>
+                <p></p>
+                <ul>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+            <div className='landing-page-content-top-areas'>
+              <span></span>
+              <h2></h2>
+              <div>
+                <div></div>
+              </div>
+            </div>
+            <div className='landing-page-content-take-tour'>
+            <span></span>
+              <h2></h2>
+              <button></button>
+            </div>
+            <div className='landing-page-content-agents'>
+              <span></span>
+              <h2></h2>
+              <div>
+                <div>
+                  <h2></h2>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            <div className='landing-page-content-become-agent'></div>
+            <div className='landing-page-content-real-estate-by-numbers'>
+              <h2></h2>
+              <p></p>
+              <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+            <div className='landing-page-content-customer-reviews'>
+              <span></span>
+              <h2></h2>
+              <div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+            <div className='landing-page-content-trending'>
+              <span></span>
+              <h2></h2>
+              <div>
+                <div></div>
+              </div>
+            </div>
+            <div className='landing-page-content-become-real-estate-agent'>
             </div>
           </div>
         </div>
